bdd48ecc35d967c23ff26e825fb7dae1d6981e6a
Ediellyf <ediellyferreiradecarvalho@gmail.com>
2024-08-25 14:38:07 -0300

    fix: correcoes botoes

---
+++ index.js
@@ -0,0 +1,7 @@
+document.querySelector('.botao-primario').addEventListener('click', ()=> {
+    window.location.href = 'https://www.netflix.com/br/title/60023642';
+})
+
+document.querySelector('.botao-secundario').addEventListener('click', ()=> {
+    window.location.href = 'https://www.youtube.com/watch?v=SgZI655GgHk';
+})
